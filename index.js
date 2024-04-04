edab8e41c1fdc095deee0c629f6f9da8c7ed5265
Hansell Ochoa <hfochoa8@gmail.com>
2024-04-03 21:48:08 -0600

    actualizacion

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 const database = require('./api-services/database')
 const webServer = require('./api-services/web-server');
+const path = require('path');
+console.log(path.sep);
 
 async function iniciar() {
+    
     try {
         process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0
         await database.initialize()
