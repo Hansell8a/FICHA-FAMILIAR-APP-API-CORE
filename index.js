07cba48c7fd2b055a7b327e39e155317d606c943
Hansell Ochoa Ortiz <hansell.ochoa@claro.com.ni>
2024-05-23 16:48:23 -0600

    solo login

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ fs.readdir(process.env.DB_CLIENT_LIB, (err, files) => {
 async function iniciar() {
     try {
         //process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0
-        await database.initialize();
+        //await database.initialize();
         await webServer.initialize();
     } catch (error) {
         console.log(error);
