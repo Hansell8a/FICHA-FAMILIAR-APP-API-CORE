d1ef5323ae7fdcf92973bb5462f79b2438a90636
Hansell Ochoa Ortiz <hansell.ochoa@claro.com.ni>
2024-05-23 10:26:15 -0600

    prueba 3

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ficha-familiar-app-api-core
+
+# Deploy
+
+unzip oracle_linux.zip ; export LD_LIBRARY_PATH=/opt/render/project/src/oracle/instantclient_19_19:$LD_LIBRARY_PATH ;
+export ORACLE_HOME=/opt/render/project/src/oracle/instantclient_19_19 ; sudo apt-get install libaio1 ; npm install
---
+++ setup_oracle.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Descomprimir Oracle Instant Client
+unzip -o oracle_linux.zip -d /opt/render/project/src/
+
+# Exportar las variables de entorno
+export LD_LIBRARY_PATH=/opt/render/project/src/oracle/instantclient_19_19:$LD_LIBRARY_PATH
+export ORACLE_HOME=/opt/render/project/src/oracle/instantclient_19_19
+
+# Instalar libaio1
+sudo apt-get update && sudo apt-get install -y libaio1
