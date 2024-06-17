befc4f5fd50b2ff6c709e39f15dfc827b01c5e72
Hansell Ochoa Ortiz <hansell.ochoa@claro.com.ni>
2024-06-17 17:08:53 -0600

    actualizacion

--- controllers/reportesController.js
+++ controllers/reportesController.js
@@ -1,20 +1,40 @@
 var express = require('express');
 var router = express.Router();
 const { CODE } = require('../common/http-status-code');
-var services = require("../services/reportesServices");
+var responseHttp = require("../common/response-template");
+const fetch = require('node-fetch');
 const {
     manejarErrorRequest
 } = require('../common/error-handler');
 
 router.get('/', (req, res, next) => {
-    services.obtenerReporteFichaFamiliar(req,req.query,"GET").then((response) => {
-        if(response.status != CODE.OK){res.status(response.status).send(manejarErrorRequest(response));} 
-        else {res.status(response.status).send(response);}
-    }, (error) => {
-        res.status(CODE.INTERNAL_SERVER_ERROR).send(manejarErrorRequest(error));        
-    }).catch((ex) => {
-        res.status(CODE.INTERNAL_SERVER_ERROR).send(manejarErrorRequest(ex));  
-    });
+    try {
+        const id_ficha_familiar = req.query.id_ficha_familiar;
+        const URL = `${process.env.REPORT_URL}/reporte?rutaReporte=SALUD_FAMILIAR/Registro_Familiar_Comunitario.pdf?pfichaFamiliar=${id_ficha_familiar}`;
+        fetch(URL, {
+            headers: {
+                'Authorization': 'Basic cmVwb3J0ZXJpYXNtRlcmlh'
+            }
+        }).then(resp => {
+            if (!resp.ok) {
+                res.status(CODE.INTERNAL_SERVER_ERROR).send(manejarErrorRequest(null)); 
+            }
+            return resp.buffer(); // Obtiene el contenido del archivo como un buffer
+        }).then(buffer => {
+            responseHttp.status = CODE.OK;
+            responseHttp.success = true;
+            responseHttp.message = 'Archivo obtenido con exito';
+            responseHttp.error =  '';
+            responseHttp.data = buffer;
+            
+            res.setHeader('Content-Disposition', `attachment; filename="ficha_Familiar.pdf"`);
+            res.setHeader('Content-Type', 'application/pdf');
+            res.status(CODE.OK).send(buffer);
+            //res.buffer();
+        })
+    }catch (ex) {
+        res.status(CODE.INTERNAL_SERVER_ERROR).send(manejarErrorRequest(ex)); 
+    }
 });
 
 module.exports = router;
--- services/reportesServices.js
+++ services/reportesServices.js
@@ -1,43 +1,42 @@
 var responseHttp = require("../common/response-template");
 const { CODE } = require('../common/http-status-code');
-const { response } = require("express");
+const fetch = require('node-fetch'); 
 
 exports.obtenerReporteFichaFamiliar = (req, parametros, method) => {
     return new Promise((resolve, reject) => {
-
         try {
-            const URL = `https://sideasgoe.mspas.gob.gt:8465/reporte?rutaReporte=SALUD_FAMILIAR/Registro_Familiar_Comunitario.pdf?pfichaFamiliar=${parametros.id_ficha_familiar}`;
+            const URL = `https://sideasqa.mspas.gob.gt:8465/reporte?rutaReporte=SALUD_FAMILIAR/Registro_Familiar_Comunitario.pdf?pfichaFamiliar=${parametros.id_ficha_familiar}`;
             fetch(URL, {
-                headers: new Headers({
+                headers: {
                     'Authorization': 'Basic cmVwb3J0ZXJpYXNtRlcmlh'
-                })
-            }).then(resp => resp.blob()).then(blob => {
-                console.log(blob);
-                const url = window.URL.createObjectURL(blob);
-                //const a = document.createElement('a');
-                //a.style.display = 'none';
-                //a.href = url;
-                // the filename you want
-                //a.download = `ficha_Familiar-${id_ficha_familiar}.pdf`;
-                //document.body.appendChild(a);
-                //a.click();
-                window.URL.revokeObjectURL(url);
-                console.log(url);
-
+                }
+            })
+            .then(resp => {
+                if (!resp.ok) {
+                    responseHttp.status = CODE.INTERNAL_SERVER_ERROR;
+                    responseHttp.success = false;
+                    responseHttp.message = 'Error al obtener el archivo';
+                    responseHttp.error =  '';
+                    responseHttp.data = [];
+                    return reject(responseHttp);
+                }
+                return resp.buffer(); // Obtiene el contenido del archivo como un buffer
+            })
+            .then(buffer => {
                 responseHttp.status = CODE.OK;
                 responseHttp.success = true;
-                responseHttp.message = '';
+                responseHttp.message = 'Archivo obtenido con exito';
                 responseHttp.error =  '';
-                responseHttp.data = url;
-
-                return response(responseHttp);
-
-                //hideSpinner();
-            }).catch((e) => {
+                responseHttp.data = [];
+                return resolve({
+                    responseHttp, 
+                buffer});
+            })
+            .catch((e) => {
                 responseHttp.status = CODE.INTERNAL_SERVER_ERROR;
                 responseHttp.success = false;
-                responseHttp.message = URL;
-                responseHttp.error =  e;
+                responseHttp.message = '';
+                responseHttp.error =  'Error al obtener el reporte:', e.messaje;
                 responseHttp.data = [];
                 return reject(responseHttp);
             });
